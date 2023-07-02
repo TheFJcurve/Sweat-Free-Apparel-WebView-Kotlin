7fd09130101e3abea9ffa28e0b645b80194ee8f3
Sargun <72266893+TheFJcurve@users.noreply.github.com>
2023-07-02 12:52:18 -0400

    smart toolbar

--- app/src/main/java/com/example/sfawebview/MainActivity.kt
+++ app/src/main/java/com/example/sfawebview/MainActivity.kt
@@ -3,17 +3,30 @@ package com.example.sfawebview
 // This is The MainActivity file of the project. Here all the compilation, JavaScript coding
 // and button functionality is written.
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.WindowManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
+
 
 // In this document you will see Deprecation and Deprecated In Java Terms thrown around.
 // They do not mean any error or harm. These are simply those modules or methods that are
@@ -23,6 +36,7 @@ import com.google.firebase.FirebaseApp
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
     private lateinit var toolbar: Toolbar
+//    private lateinit var appBarLayout: AppBarLayout
     private val homeUrl = "https://www.sweatfree.co" // Our main Website URL
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -30,21 +44,73 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Register for Firebase Cloud Messaging
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get the token
+            val token = task.result
+
+            // TODO: Send the token to your server or handle it as required
+            Log.d(TAG, "FCM registration token: $token")
+        })
+
+
         webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
         toolbar = findViewById(R.id.toolbar)
+
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                if (url != null && (!url.startsWith(homeUrl) || url == homeUrl)) {
+                    supportActionBar?.show() // Show the toolbar for external websites
+                } else {
+                    supportActionBar?.hide() // Hide the toolbar for the home page
+                }
+            }
+        }
+
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         // Setting up the WebView. This shows our website in the app. Below I have made it possible
         // to zoom and write JavaScript code (to make buttons and write other commands).
         // If you wish to change the website displayed, just change the homeUrl variable, or
         // input the url you wish to see in .loadUrl()
-        webView.webViewClient = WebViewClient()
         webView.loadUrl(homeUrl)
         webView.settings.setSupportZoom(true)
         webView.settings.javaScriptEnabled = true
 
+        // Setting up a dynamic toolbar, which hides when we are scrolling through the app.
+        // It also automatically comes back up when needed.
+        // appBarLayout = findViewById(R.id.appBarLayout)
+
+//        webView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+//            if (scrollY < oldScrollY) {
+//                appBarLayout.visibility = View.GONE // Hide the AppBarLayout
+//            } else {
+//                appBarLayout.visibility = View.VISIBLE // Show the AppBarLayout
+//            }
+//        }
+
         // Checking if Firebase works
         FirebaseApp.initializeApp(this)
+
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        // Check if the activity was opened from a notification
+        val intent = intent
+        if (intent.extras != null) {
+            // Handle the notification data here
+            val notificationData = intent.extras
+            // TODO: Handle the notification data as required
+        }
+
     }
 
     // menu_main.xml contains all the information regarding a button (the button icon, positioning,
@@ -53,6 +119,63 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
+
+    // Setting up all of the functions
+
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: Display an educational UI explaining to the user the features that will be enabled
+                // by granting the POST_NOTIFICATION permission. This UI should provide the user with
+                // "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                // If the user selects "No thanks," allow the user to continue without notifications.
+
+                // Create and show an AlertDialog with the educational UI
+                val dialogBuilder = AlertDialog.Builder(this)
+                    .setTitle("Notification Permission")
+                    .setMessage("Granting the notification permission allows you to receive important updates and notifications.")
+                    .setPositiveButton("OK") { dialog, _ ->
+                        // Request the permission when the user selects "OK"
+                        requestNotificationPermission()
+                        dialog.dismiss()
+                    }
+                    .setNegativeButton("No thanks") { dialog, _ ->
+                        // Handle "No thanks" action, allow the user to continue without notifications
+                        dialog.dismiss()
+                    }
+
+                val dialog = dialogBuilder.create()
+                dialog.show()
+            } else {
+                // Directly ask for the permission
+                requestNotificationPermission()
+            }
+        }
+    }
+
+    private fun requestNotificationPermission() {
+        // Request the POST_NOTIFICATIONS permission using the permission launcher
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    }
+
 
     // When a button is clicked, this function calls the designated button function on it. Here
     // we link a button with the function.
@@ -89,8 +212,6 @@ class MainActivity : AppCompatActivity() {
 
     // Setting up button functions
 
-
-
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         // Go to the previously shown page each time you click, until can't go back.
@@ -126,7 +247,14 @@ class MainActivity : AppCompatActivity() {
             val shareIntent = Intent(Intent.ACTION_SEND)
             shareIntent.type = "text/plain"
             shareIntent.putExtra(Intent.EXTRA_TEXT, currentUrl)
-            startActivity(Intent.createChooser(shareIntent, "Share Link"))
+
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(currentUrl))
+
+            // Create a chooser with both the share intent and the browser intent
+            val chooserIntent = Intent.createChooser(shareIntent, "Share Link")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(browserIntent))
+
+            startActivity(chooserIntent)
         } else {
             Toast.makeText(
                 this, "Unable to get current URL", Toast.LENGTH_SHORT).show()
@@ -137,5 +265,4 @@ class MainActivity : AppCompatActivity() {
         // Reloads page
         webView.reload()
     }
-
 }
--- app/src/main/java/com/example/sfawebview/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/sfawebview/MyFirebaseMessagingService.kt
@@ -4,13 +4,15 @@ import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.widget.RemoteViews
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+
 
 const val channelId = "notification_channel"
 
@@ -18,29 +20,20 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        // Handle the new token here if needed
+        // Handle the new device token here
+        // You can send the token to your server or perform any necessary operations
+        // For testing purposes, you can log the token to verify that it is being retrieved correctly
+        Log.d(TAG, "New Token: $token")
     }
 
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        if (remoteMessage.getNotification() != null) {
+        if (remoteMessage.notification != null) {
             showNotification(
                 remoteMessage.notification!!.title!!,
                 remoteMessage.notification!!.body!!
             )
         }
-    }
-
-    private fun getCustomDesign(title: String, message: String): RemoteViews {
-        val remoteView = RemoteViews(
-            "com.example.sfawebview",
-            R.layout.notification_main
-        )
-
-        remoteView.setTextViewText(R.id.title, title)
-        remoteView.setTextViewText(R.id.message, message)
-        remoteView.setImageViewResource(R.id.app_logo, R.drawable.sweatfree_logo_round)
-
-        return remoteView
     }
 
     @SuppressLint("UnspecifiedImmutableFlag")
@@ -55,26 +48,26 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         )
 
         var builder: NotificationCompat.Builder = NotificationCompat.Builder(
-            applicationContext,
+            this,
             channelId
         )
             .setSmallIcon(R.drawable.sweatfree_logo_round)
             .setAutoCancel(true)
+            .setContentTitle(title)
+            .setContentText(message)
             .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
             .setOnlyAlertOnce(true)
             .setContentIntent(pendingIntent)
 
-        builder = builder.setContent(getCustomDesign(title, message))
-
         val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel =
                 NotificationChannel(channelId, "Sweat Free", NotificationManager.IMPORTANCE_HIGH)
-            notificationManager.createNotificationChannel(notificationChannel)
+            notificationManager!!.createNotificationChannel(notificationChannel)
         }
 
-        notificationManager.notify(0, builder.build())
+        notificationManager!!.notify(1, builder.build())
     }
 }
