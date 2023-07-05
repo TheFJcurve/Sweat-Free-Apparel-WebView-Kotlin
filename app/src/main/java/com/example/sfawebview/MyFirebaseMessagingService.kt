98aed157de02ddcf0951c8e90d474281d3bcfd94
Sargun <72266893+TheFJcurve@users.noreply.github.com>
2023-07-05 19:15:38 -0400

    firebase working

--- app/src/main/java/com/example/sfawebview/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/sfawebview/MyFirebaseMessagingService.kt
@@ -2,8 +2,10 @@ package com.example.sfawebview
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -13,6 +15,7 @@ import com.google.firebase.messaging.RemoteMessage
 
 const val channelId = "notification_channel"
 
+@Suppress("NAME_SHADOWING")
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
@@ -24,15 +27,22 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        if (remoteMessage.getNotification() != null) {
+        if (remoteMessage.notification != null) {
             showNotification(
-                remoteMessage.getNotification()!!.getTitle()!!,
-                remoteMessage.getNotification()!!.getBody()!!
+                remoteMessage.notification!!.title!!,
+                remoteMessage.notification!!.body!!
             )
         }
     }
 
     private fun showNotification(title: String, message: String) {
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         val builder: NotificationCompat.Builder = NotificationCompat.Builder(
             this,
             channelId
@@ -44,14 +54,23 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
             .setOnlyAlertOnce(true)
 
+        builder.setContentIntent(pendingIntent)
+
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
 
+        // Inside your activity or initialization class
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel =
-                NotificationChannel(channelId, "Sweat Free", NotificationManager.IMPORTANCE_HIGH)
-            notificationManager!!.createNotificationChannel(notificationChannel)
+            val channelName = "Sweat Free" // Name of the channel
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = "Channel description" // Optional description for the channel
+            }
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
+
 
         notificationManager!!.notify(0, builder.build())
     }
